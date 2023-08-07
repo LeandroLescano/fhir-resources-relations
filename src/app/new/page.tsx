55e9733d2215e11536431ad50b650e6018a5aea4
Marcos Monaco <macmonaco20002@gmail.com>
2023-08-07 17:42:54 -0300

    progreso con los graphs

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,66 +1,96 @@
 "use client";
+
 import React, {useEffect, useRef, useState} from "react";
 import {DataSet} from "vis-data";
-import {IdType, Network, Options} from "vis-network";
+import {Edge, IdType, Network, Node, Options} from "vis-network";
+import {useRouter} from "next/navigation";
 import Head from "next/head";
-import Link from "next/link";
+
+import {getFHIRResources} from "src/utils/processText";
 
 const Graph = () => {
-  const [selectedNode, setSelectedNode] = useState<{[key: string]: string}>({});
-  const network = useRef<Network | null>(null);
   const nodes = useRef<DataSet<any, any>>(new DataSet());
   const edges = useRef<DataSet<any, any>>(new DataSet());
-  const nombre = useRef<DataSet<any, any>>(new DataSet());
-  const visJsRef = useRef<HTMLDivElement>(null);
+  const [selectedNode, setSelectedNode] = useState<{[key: string]: string}>({});
+  const [openAddDialog, setOpenAddDialog] = useState(false);
   const [loading, setLoading] = useState(false);
+  const network = useRef<Network | null>(null);
   const highlightActive = useRef(false);
-  const [graphData, setGraphData] = useState(null);
+  const router = useRouter();
+
+  const visJsRef = useRef<HTMLDivElement>(null);
 
   const getData = async () => {
-    const savedGraphDataString = localStorage.getItem("graphs");
-    if (savedGraphDataString) {
-      const savedGraphData = JSON.parse(savedGraphDataString);
+    const resources = getFHIRResources();
 
-      let nodesAux = [];
-      let edgesAux = [];
-      let nombreAux = [];
-      let index = 1;
+    const resourcesNodes = resources.map((res) => ({
+      label: res.resourceType,
+      id: res.resourceType,
+      group: "1",
+    }));
 
-      setGraphData(savedGraphData);
+    nodes.current.clear();
+    nodes.current.update(resourcesNodes);
 
-      for (let atomo of savedGraphData) {
-        nombreAux.push({
-          nombre: atomo.nombre,
-          group: index,
-        });
+    const resourcesEdges = [];
 
-        nodesAux.push(
-          ...atomo.nodes.map((node: any) => ({
-            ...node,
-            group: index,
-            id: node.id + index,
-          }))
-        );
-        edgesAux.push(
-          ...atomo.edges.map((edge: any) => ({
-            ...edge,
-            group: index,
-            from: edge.from + index,
-            to: edge.to + index,
-          }))
-        );
-        index++;
+    for (const res of resources) {
+      for (const props of Object.entries(res)) {
+        const edgeLabel = props[0];
+        const connections = props[1];
+        if (connections) {
+          if (typeof connections !== "string") {
+            for (const connection of connections as string[]) {
+              resourcesEdges.push({
+                from: res.resourceType,
+                to: connection,
+                label: edgeLabel,
+              });
+            }
+          }
+        }
       }
-
-      nodes.current.clear();
-      edges.current.clear();
-      nombre.current.clear();
-
-      nodes.current.update(nodesAux);
-      edges.current.update(edgesAux);
-      nombre.current.update(nombreAux);
     }
+    const groupedEdges: any[] = [];
+    for (const edge of resourcesEdges) {
+      const findedEdge = groupedEdges.find(
+        (e) => e.from === edge.from && edge.to === e.to
+      );
+      if (findedEdge) {
+        findedEdge.label += ", " + edge.label;
+      } else {
+        groupedEdges.push(edge);
+      }
+    }
+    console.log({groupedEdges});
+    edges.current.clear();
+    edges.current.update(groupedEdges);
   };
+
+  function saveGraph() {
+    const graphData = {
+      nombre: prompt("Ingresa el nombre del grupo"),
+      nodes: nodes.current.get({returnType: "Array"}),
+      edges: edges.current.get({returnType: "Array"}),
+    };
+
+    // Retrieve existing graphs (if any) from local storage
+    const existingGraphsString = localStorage.getItem("graphs");
+    let existingGraphs = existingGraphsString
+      ? JSON.parse(existingGraphsString)
+      : [];
+
+    // Add the new graph data (as an object) to the array of existing graphs
+    existingGraphs.push(graphData);
+
+    // Save the updated array of graphs to local storage
+    const updatedGraphsString = JSON.stringify(existingGraphs);
+    localStorage.setItem("graphs", updatedGraphsString);
+
+    // Redirect to the main page
+
+    router.push("/fhir");
+  }
 
   useEffect(() => {
     const options: Options = {
@@ -106,6 +136,28 @@ const Graph = () => {
         stabilization: {iterations: 150},
         hierarchicalRepulsion: {
           avoidOverlap: 2,
+        },
+      },
+      manipulation: {
+        deleteEdge: async function (data: any, callback: any) {
+          let edgeFind = edges.current
+            .get({returnType: "Array"})
+            .find((e) => e.id === data.edges[0]);
+          if (edgeFind) {
+            let fromObj = nodes.current
+              .get({returnType: "Array"})
+              .find((n) => n.id === edgeFind.from);
+            let toObj = nodes.current
+              .get({returnType: "Array"})
+              .find((n) => n.id === edgeFind.to);
+            const res = confirm(
+              `Quieres remover la relación seleccionada? \n SOURCE: ${fromObj.label} \n\n TARGET:  ${toObj.label}`
+            );
+            if (res == true) {
+              edges.current.remove(data.edges[0]);
+            }
+            callback();
+          }
         },
       },
       edges: {
@@ -358,10 +410,6 @@ const Graph = () => {
         />
       </div>
       <div>
-        <Link passHref href="/new">
-          <button>Agregar nuevo grupo</button>
-        </Link>
-
         <label>
           <input
             type="checkbox"
@@ -373,21 +421,8 @@ const Graph = () => {
           />{" "}
           Habilitar físicas
         </label>
-      </div>
-      <div>
-        {graphData.nombre.map((name) => (
-          <div key={name}>
-            <label>
-              <input
-                type="checkbox"
-                value={name}
-                // checked={graphNames.includes(name)}
-                // onChange={handleCheckboxChange}
-              />
-              {name}
-            </label>
-          </div>
-        ))}
+        <label> </label>
+        <button onClick={saveGraph}>Guardar</button>
       </div>
     </>
   );
